6dc5d083435e24da466bb7e06c3a816616efbc22
migueltoro <migueltoro@pc01>
2024-10-19 11:38:25 +0200

    19/10/2024

--- Python_v1/us/lsi/ejemplos_b2/Ejemplos_comprension.py
+++ Python_v1/us/lsi/ejemplos_b2/Ejemplos_comprension.py
@@ -14,40 +14,62 @@ import re
 def test0():
     print(encoding(absolute_path('/resources/lin_quijote.txt')))
     
+'''
+Comparación entre la forma clásica , explícita y por comprensión de definir colecciones
+
+'''
+    
 def test1():
+    # Forma clásica
     for x in range(3, 70):
         if x % 3 == 0:
             t = x**2
             pass
-        
+    # Forma por comprensión   
     r:Iterable[int] = (x**2 for x in range(3, 70) if x % 3 == 0)
     
     for e in r:
         print(e)
     
+    # Forma por explícita
+    r2: Iterable[int] = (3,56,78,67,45)
+    for e in r2:
+        print(e)
+    
 def test2():
+    # Forma clásica
     sr: list[int] = []
     for x in range(3, 70):
         if x % 3 == 0:
             t = x**2
             sr.append(t)
 
+    # Forma por comprensión 
     s:list[int] = [x**2 for x in range(3, 70) if x % 3 == 0]
     print(s)
     print(sr)
+    
+    # Forma por explícita
+    s2: list[int] = [3,56,78,67,45]
         
 def test3():
+    # Forma clásica
     sts: set[int] = set()
     for x in range(3, 70):
         if x % 3 == 0:
             t = x**2
             sts.add(t)
-        
+    
+    # Forma por comprensión     
     st:set[int] = {x**2 for x in range(3, 70) if x % 3 == 0}
     print(sts)
     print(st)
+    # Forma por explícita
+    st2: set[int] = {3,56,78,67,45}
+    print(st2)
     
 def test4():
+    # Forma clásica
     dtr: dict[int,int] = {}
     for x in range(3, 70):
         if x % 3 == 0:
@@ -55,14 +77,27 @@ def test4():
             t2 = x**3
             dtr[t1] = t2
 
+    # Forma por comprensión 
     dt:dict[int,int] = {x**2:x**3 for x in range(3, 70) if x % 3 == 0}
     print(dtr)
     print(dt)
+    # Forma por explícita
+    dt2: dict[int,int] = {3:56,78:67,45:45}
+    print(dt2)
+    
+'''
+Usos 
+'''
+
     
 def test5():
     nombres:list[str] = ["Miguel", "Ana", "Jose Maria", "Guillermo", "Maria", "Luisa"]
     ranking:dict[str,int] = {nombre: nombres.index(nombre) for nombre in nombres}
     print(f'ranking = {ranking}')
+    
+'''
+Funciones para trasnformar iterables
+'''    
 
 def test6():  
     texto:str = "este es un pequeño texto para probar la siguiente definicion por comprension"
@@ -72,19 +107,19 @@ def test6():
     print(f'palabras_por_iniciales = {palabras_por_iniciales}')
     
 def test7():
-    fichero:str= absolute_path("/datos/datos_3.txt")
+    fichero:str= absolute_path("datos/datos_3.txt")
     it3:Iterable[list[str]] = lineas_de_csv(fichero)
-    it4:Iterable[str] = flat_map(it3,key=lambda x:x)
+    it4:Iterable[str] = flat_map(it3)
     it5:Iterable[int] = map(lambda e:int(e),it4)
     it6:Iterable[int] = distinct(it5)
     r0:list[int] = sorted(it6,key=lambda x:x)
     print(f'sorted = {r0}')
     
 def test8():
-    r7:Iterable[str] = flat_map(iterable_de_fichero(absolute_path("/datos/datos_3.txt")),key=lambda ln: re.split(',',ln))
+    r7:Iterable[str] = flat_map(iterable_de_fichero(absolute_path("datos/datos_3.txt")),key=lambda ln: re.split(',',ln))
     print(list(r7))
     print('________________')
-    r8:Iterable[str] = flat_map(lineas_de_csv(absolute_path('/resources/lin_quijote.txt'), encoding='ISO-8859-1',delimiter=' '))
+    r8:Iterable[str] = flat_map(lineas_de_csv(absolute_path('resources/lin_quijote.txt'), encoding='ISO-8859-1',delimiter=' '))
     r9:Iterable[str] = filter(lambda x: len(x)>0,r8)
     print(list(r9))
     
@@ -127,5 +162,5 @@ def test13():
     
 
 if __name__ == '__main__':
-    test10()
+    test13()
     
