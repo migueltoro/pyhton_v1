f99d14221b377be3ffb95b1daf40db28b559a800
migueltoro <migueltoro@pc01>
2025-12-16 20:25:59 +0100

    16/12/2025

---
+++ Python_v1/us/lsi/questions/Questions.py
@@ -0,0 +1,128 @@
+'''
+Created on 16 dic 2025
+
+@author: migueltoro
+'''
+
+'''
+     * Problema 1
+     * 
+     * Invierte un mapa, agrupando las claves originales en listas según su valor.
+     * Si varias claves tienen el mismo valor, todas se agrupan en la lista correspondiente a ese valor.
+     *
+     * @param <K> el tipo de las claves del mapa original
+     * @param <V> el tipo de los valores del mapa original (y claves del mapa invertido)
+     * @param m el mapa a invertir
+     * @return un nuevo mapa donde cada valor original es clave y su valor es la lista de claves originales que tenían ese valor
+     */
+    
+    /**
+     * Problema 2
+     * 
+     * Invierte un mapa, agrupando las claves originales en cojuntos según su valor.
+     * Si varias claves tienen el mismo valor, todas se agrupan en el conjunto correspondiente a ese valor.
+     *
+     * @param <K> el tipo de las claves del mapa original
+     * @param <V> el tipo de los valores del mapa original (y claves del mapa invertido)
+     * @param m el mapa a invertir
+     * @return un nuevo mapa donde cada valor original es clave y su valor es el conjunto de claves originales que tenían ese valor
+     */
+    
+    /**
+    * Problema 3
+    * 
+    * Este método toma un mapa de elementos y sus colores asociados (representados como enteros), 
+    * y un conjunto de pares que representan elementos vecinos.
+    * Devuelve un mapa donde cada elemento está asociado a un conjunto de colores de sus elementos vecinos.
+    *
+    * @param <E> el tipo de elementos
+    * @param colores un mapa donde las claves son elementos y los valores son sus colores asociados
+    * @param vecinos un conjunto de pares que representan elementos vecinos
+    * @return un mapa donde cada elemento está asociado a un conjunto de colores de sus elementos vecinos
+    */
+    
+    
+    /**
+    * Une dos flujos basándose en una clave común extraída de cada elemento.
+    *
+    @param el tipo de elementos del primer flujo
+    @param el tipo de elementos del segundo flujo
+    @param el tipo de clave utilizada para la unión
+    @param s1 el primer flujo
+    @param s2 el segundo flujo
+    @param k1 una función para extraer la clave de los elementos del primer flujo
+    @param k2 una función para extraer la clave de los elementos del segundo flujo
+    @return un flujo de pares, donde cada par consta de un elemento del primer flujo y 
+        un elemento del segundo flujo que comparten la misma clave
+    */
+    
+    /**
+    * Problema 4
+    * 
+    * Enumera los elementos de un flujo a partir de un entero dado.
+    *
+    * @param <E> el tipo de elementos del flujo
+    * @param stream el flujo de entrada que se enumerará
+    * @param start el entero inicial para la enumeración
+    * @return un flujo de objetos Enumerate donde cada elemento se empareja con su índice de enumeración
+    */
+    
+    /**
+    * 
+    * Problema 5
+    * Genera el producto cartesiano de dos flujos.
+    *
+    @param el tipo de elementos del primer flujo
+    @param el tipo de elementos del segundo flujo
+    @param s1 el primer flujo
+    @param s2 el segundo flujo
+    @return un flujo de pares, donde cada par consta de un elemento del primer flujo y un elemento del segundo flujo
+    */
+    
+    /**
+    * Problema 6
+    * 
+    * Genera un flujo de pares consecutivos a partir del flujo de entrada.
+    *
+    * @param <T> el tipo de elementos en el flujo de entrada
+    * @param s el flujo de entrada
+    * @return un flujo de pares donde cada par consta de dos elementos consecutivos del flujo de entrada
+    */
+    
+    /**
+     * Problema 7
+     * 
+     * Devuelve un mapa donde cada elemento del flujo está asociado con una lista ordenada
+     * de los elementos que están a una distancia superior a dmin.
+     *
+     * @param <E> el tipo de los elementos en el flujo
+     * @param stream el flujo de elementos
+     * @param d la función que calcula la distancia entre dos elementos
+     * @param dmin la distancia mínima
+     * @return un mapa donde cada elemento está asociado con una lista ordenada de elementos
+     *         cuya distancia es superior a dmin
+     */
+    
+    /**
+     * Problema 8
+     * 
+     * Descarga un fichero de texto desde una URL y lo guarda en un fichero local.
+     *
+     * @param fileUrl la URL del fichero remoto
+     * @param localFilePath la ruta del fichero local donde se guardará
+     * @throws IOException si ocurre un error al leer o escribir el fichero
+     */
+    
+    /**
+     * Descarga un fichero de una Url, lo guarda en un fichero local y lo convierte en una lista de cadenas.
+     *
+     * @param fileUrl la URL del fichero remoto
+     * @param localFilePath la ruta del fichero local donde se guardará
+     * @return una lista de cadenas con las líneas del fichero
+     * @throws IOException si ocurre un error al descargar o leer el fichero
+     */
+
+'''
+
+if __name__ == '__main__':
+    pass
